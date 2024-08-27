67d0797018db8cc872591bb0e32ad85e9b1395d5
kmfiguerrez <kmfiguerrez@gmail.com>
2024-08-27 15:45:01 +0800

    Install electron server package.
    - Added code and comments in the main.js script.

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,14 +1,70 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const serve = require("electron-serve");
+
+
+/*
+  Serve the Next.js generated static files in the out dir.
+  See: https://github.com/sindresorhus/electron-serve#readme
+*/
+const appServe = app.isPackaged ? serve({
+  directory: path.join(__dirname, "../../out")
+}) : null;
+
+let mainWindow
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  win.loadFile('index.html')
+  // For testing vanilla html.
+  // mainWindow.loadFile(path.join(__dirname, 'index.html'))
+
+  if (app.isPackaged) {
+    appServe(mainWindow)
+    .then(() => {
+      mainWindow.loadURL("app://-");
+    });
+  } 
+  else {
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+  }
 }
 
+/*
+  This method will be called when Electron has finished
+  initialization and is ready to create browser windows.
+  Some APIs can only be used after this event occurs.
+*/
 app.whenReady().then(() => {
   createWindow()
+
+  /*
+    On macOS it's common to re-create a window in the app when the
+    dock icon is clicked and there are no other windows open.
+  */
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })  
 })
+
+/*
+  Quit when all windows are closed, except on macOS. There, it's common
+  for applications and their menu bar to stay active until the user quits
+  explicitly with Cmd + Q.
+*/
+app.on("window-all-closed", () => {
+  if(process.platform !== "darwin"){
+      app.quit();
+  }
+});
+
+/*
+  In this file you can include the rest of your app's specific main process
+  code. You can also put them in separate files and require them here.
+*/
